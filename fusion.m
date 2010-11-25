76d58916c4dff830144537c25de0c8ec43c0376f
Adriano Melo <github@adrianomelo.com>
2010-11-25 13:15:05 -0300

    doc about the pyramid algorithm is comented in the code.

--- fusion.m
+++ fusion.m
@@ -30,7 +30,7 @@ function peso = saturacao(imgs)
         peso_img = ((r - media).^2 + (g - media).^2 + (r - media).^2) / 3.;
         peso(:,:,i) = sqrt(peso_img);
     end
-endfunction
+end
 
 function peso = exposicao(imgs)
     t = size(imgs);
@@ -51,12 +51,25 @@ function peso = exposicao(imgs)
     end
 end
 
-function M = piramide_laplaciana(img)
-    % 1 - Compute a reduced-resolution approximation of the input image.
+% Algoritmo de construção da pirâmide (Gonzalez, 308)
 
-    
+% O procedimento de tres passos a seguir é executado P vezes, para j = J, J-1, ... e J-P+1:
 
+    % Passo 1: Calcule uma aproximação de resolução reduzida da imagem de entrada de nível j.
+    % Isso é feito pela filtragem e subamostragem (downsampling) do resultado filtrado por um fator de 2.
+    % Posicione a aproximação resultante no nível j-1 da pirâmide de aproximação.
+
+    % Passo 2: Crie uma estimativa da imagem de nível j a partir da aproximação de resolução reduzida gerana no passo 1.
+    % Isso é feito pela superamostragem (upsampling) e filtragem da aproximação gerada.
+    % A imagem preditiva resultante terá as mesmas dimensões que a imagem de entrada de nível j.
+
+    % Passo 3: Calcule a diferença entre a imagem preditiva do passo 2 e a entrada do passo 1. 
+    % Coloque esse resultado no nível j da pirâmide de residual de previsão.
+
+% No final das P repetições (isto é, após a iteração na qual j = J - P + 1), a seída da aproximação de nível J - P
+%  é colocada na pirâmide de residual de previsão no nível J - P.
+
+function M = piramide(img)
 
 end
-
 
